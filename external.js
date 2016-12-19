01438e0253386c9b8d822597388a42d9fb1f0b77
Prabhath565 <prabhath565@gmail.com>
2016-12-19 12:41:33 +0530

    S5_4

---
+++ external.js
@@ -0,0 +1,29 @@
+var Employee1 = {
+
+	Employee_Name: {
+		Name: "John",
+	},
+	Employee_Age: {
+		Age: 24,
+	},
+	Employee_salary: {
+		salary:30000,
+	},
+	Employee_city: {
+		City: "Hyderabad",
+	},
+	Employee_state: {
+		state: "Telengana",
+	},
+	Employee_Pincode: {
+		Pincode:500045,
+	},
+		
+};
+
+			console.log(Employee1.Employee_Name);
+			console.log(Employee1.Employee_Age);
+			console.log(Employee1.Employee_salary);
+			console.log(Employee1.Employee_city);
+			console.log(Employee1.Employee_state);
+			console.log(Employee1.Employee_Pincode);
